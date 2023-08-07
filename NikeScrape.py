3388a98da16b0e8a206ef649071ae5438e374d23
gededhani <dhanipradipta777@gmail.com>
2023-08-07 11:27:16 +0800

    kommit

--- NikeScrape.py
+++ NikeScrape.py
@@ -25,33 +25,37 @@ def main():
             link_url = item.locator('//a[@class="product-card__link-overlay"]').get_attribute('href')
             data_url.append(link_url)
 
+        print(len(data_url))
+        for data in data_url:
+            print(data)
+
         # scrape detail item
-        data_item = []
-        for link in data_url:
-            browser = p.chromium.launch()
-            page = browser.new_page()
-            page.goto(link)
-            page.wait_for_selector("div#__next")
-            time.sleep(5)
-            try:
-                nama = page.locator('//h1[@data-test="product-title]').inner_text()
-                harga = page.locator('//div[@data-test="product-price"]').inner_text()
-                size = page.locator('//label[@class="css-xf3ahq"]').inner_text()
-            except:
-                nama = 'none'
-                harga = 'none'
-                size = 'none'
-
-            list_item = {
-                'Nama':nama,
-                'Harga':harga,
-                'Size':size,
-            }
-            data_item.append(list_item)
-
-        #save file
-        df = pd.DataFrame(data_item)
-        print(df)
+        # data_item = []
+        # for link in data_url:
+        #     browser = p.chromium.launch()
+        #     page = browser.new_page()
+        #     page.goto(link)
+        #     page.wait_for_selector("div#__next")
+        #     time.sleep(5)
+        #     try:
+        #         nama = page.locator('//h1[@data-test="product-title]').inner_text()
+        #         harga = page.locator('//div[@data-test="product-price"]').inner_text()
+        #         size = page.locator('//label[@class="css-xf3ahq"]').inner_text()
+        #     except:
+        #         nama = 'none'
+        #         harga = 'none'
+        #         size = 'none'
+        #
+        #     list_item = {
+        #         'Nama':nama,
+        #         'Harga':harga,
+        #         'Size':size,
+        #     }
+        #     data_item.append(list_item)
+        #
+        # #save file
+        # df = pd.DataFrame(data_item)
+        # print(df)
 
         browser.close()
 
